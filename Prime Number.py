373784fd507e200df3bcb9ed2326d4990f526a88
lloja1261 <lloja1261@bths.edu>
2017-03-25 15:32:39 -0400

    Add files via upload

---
+++ Prime Number.py
@@ -0,0 +1,38 @@
+#Luis Loja
+#Program 4: Determining a Prime Number
+#November 25, 2016
+
+
+ans="y"
+while ans=="y":
+    number=0
+    summ=0
+    n=int(input("Enter a positive integer: "))
+    prime="yes"
+    for i in range (2,int(n**0.5)+1):
+        if (n%i)==0:
+            print(n,"is not prime")
+            prime="no"
+            break
+        
+    if prime =="yes":
+        print(n,"is prime!")
+
+    if prime=="no":
+        for i in range(1,n+1):
+            if (n%i)==0:
+                summ+=i
+                number+=1
+                print(i,"is a divisor of",n)
+        print("The sum of all the divisors for",n,"is",summ)
+        print("There are",number,"divisors for",n)
+    
+    ans=input("Do you want to do this again. Type (y)es or (n)o?:")
+if ans=="n":
+    print("I hope that this helped you. Bye!!!")
+
+
+
+
+
+
